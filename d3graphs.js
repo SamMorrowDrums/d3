e8576bf179d9325a144d131cf690030f80308cdb
Sam Morrow <info@sammorrowdrums.com>
2014-05-28 23:54:36 +0100

    Initial working prototype for graph generator

--- d3graphs.js
+++ d3graphs.js
@@ -1,36 +1,59 @@
 var graphs = (function (d3, document) {
   graphs = {};
 
-  function bar () {
-    var b = document.createElement('g');
-    var rec = document.createElement('rect');
-    var txt = document.createElement('text');
-    b.appendChild(rec);
-    b.appendChild(txt);
-    return b;
-  }
-
-  function Barchart (attrs, data) {
-    if (this instanceof Barchart === false) {
-      return new Barchart();
-    }
-
-    this.svg = document.createElement('svg');
-    this.svg.setAttribute('class', 'chart');
-  }
-
-  Barchart.prototype.data = function (data) {
-
+  var defaults = {
+    scale: 'linear',
+    width: 1000,
+    height: 500,
   };
 
-  Barchart.prototype.render = function (data) {
-    this.svg = this.svg.cloneNode();
-    this.svg.appendChild(bar());
-    return this.svg;
+  function size (chart, w, h) {
+    w = w || defaults.width;
+    h = h || defaults.height;
+    chart
+      .attr("width", w)
+      .attr("height", h);
+  }
+
+  function bars (chart, data, barWidth, y, h) {
+    h = h || defaults.height;
+    chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; }  )
+    .append("rect")
+      .attr("y", function(d) { return y(d); })
+      .attr("height", function(d) { return h - y(d); })
+      .attr("width", barWidth - 1)
+    .append("text")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { return y(d) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d; });
+  }
+
+  function scale (d, h, s) {
+    h = h || defaults.height;
+    s = s || defaults.scale;
+
+    return d3.scale[s]()
+      .range([h, 0])
+      .domain([0, d3.max(d)]);
+  }
+
+  graphs.barchart  = function (data, attrs) {
+    var chart = d3.select(attrs.selector).append('svg'),
+        width = attrs.width || defaults.width,
+        barWidth = width/data.length,
+        y = scale(data, attrs.height, attrs.scale);
+
+    chart.attr('class', 'chart');
+
+    size(chart, attrs.width, attrs.height);
+    bars (chart, data, barWidth, y, attrs.height);
+
+    return chart;
   };
-
-  graphs.Barchart = Barchart;
-
   
   return graphs;
 })(d3, document);
--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-var data = [4, 8, 15, 16, 23, 42];
-var body = d3.select("body");
-var div = body.append("div");
-div.html("Hello, world!");
+var data = [20, 15, 17, 19, 4, 8, 15, 16, 23, 42];
+graphs.barchart(data, {selector: '.graph'});
+
+/*
+
+// We want to make a stacked bar graph
+var graph = d3.makeGraph('stacked');
+// Set the x axis labels
+graph.setPeriod(['Jan', 'Feb', 'Mar', 'Apr', 'May']);
+// Add a set of values for each stacked bar (set the label and values)
+graph.addValues('Pump 1', [125, 124, 126, 122, 126]);
+graph.addValues('Pump 2', [124, 124, 123, 122, 121]);
+graph.addValues('Pump 3', [124, 125, 124, 123, 125]);
+graph.addValues('Pump 4', [125, 126, 124, 125, 123]);
+graph.addValues('Pump 5', [123, 123, 122, 123, 124]);
+graph.addValues('Pump 6', [126, 125, 125, 126, 127]);
+// Set the colours of each of the bar types
+graph.setColours(['red','green','blue','yellow','magenta','cyan']);
+// Actually create the graphs
+graph.renderTo(body);
+
+*/
